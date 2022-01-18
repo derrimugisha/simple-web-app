e14bf0b2e54353512465dbd5921187ab31453042
derrimugisha <derrimugisha@gmail.com>
2022-01-18 17:40:18 +0300

    displaying data in the grid format,sorting also implemented

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Index = () => {
-  return <>This is working</>;
-};
-export default Index;
---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import DataGridPage from "./page-one/dataGridPage";
+
+const Index:React.FC = () => {
+  return <><DataGridPage/></>;
+};
+export default Index;
